f713483132cb0b88fc32eeb7ab48c7061526c474
Mahmodul Hasan <nicesanto92@gmail.com>
2023-12-14 13:50:25 +0600

    created a sortable list of users next target is to make it server side

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,4 +1,5 @@
 import items from "@/lib/data";
+import { Product } from "@/type";
 import Image from "next/image";
 import Link from "next/link";
 
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { faker } from "@faker-js/faker";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, CaseLower } from "lucide-react";
 import { useState } from "react";
 
 let people = faker.datatype.array(20).map(() => {
   let firstName = faker.person.firstName();
   let lastName = faker.person.lastName();
   let name = `${firstName} ${lastName}`;
-  let email = faker.internet.email(firstName, lastName).toLowerCase();
+  let email = faker.internet.email({firstName,lastName}).toLowerCase();
   return {
     name,
     title: faker.person.jobTitle(),
@@ -19,7 +19,7 @@ let people = faker.datatype.array(20).map(() => {
 const UserPage = () => {
   const [sort, setSort] = useState("");
   const [acceding, descending] = sort?.split(":") ?? [];
-  const sortedPeople = [...people].sort((a: any, b: any) => {
+  const sortedPeople = [...people]?.sort((a: any, b: any) => {
     return descending
       ? b[acceding]?.localeCompare(a[acceding])
       : a[acceding]?.localeCompare(b[acceding]);
@@ -32,55 +32,60 @@ const UserPage = () => {
         <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
           <table className="min-w-full divide-y divide-gray-200">
             <thead>
-            <tr>
-                
-              <SortableComponent
-                prop="name"
-                sort={sort}
-                onClick={(sort) => setSort(sort)}
-              >
-                Name
-              </SortableComponent>
-              <SortableComponent
-                prop="title"
-                sort={sort}
-                onClick={(sort) => setSort(sort)}
-              >
-                Title
-              </SortableComponent>
-              <SortableComponent
-                prop="title"
-                sort={sort}
-                onClick={(sort) => setSort(sort)}
-              >
-                Email
-              </SortableComponent>
-              <SortableComponent
-                prop="role"
-                sort={sort}
-                onClick={(sort) => setSort(sort)}
-              >
-                Role
-              </SortableComponent>
-            </tr>
+              <tr>
+                <SortableComponent
+                  prop="name"
+                  sort={sort}
+                  onClick={(sort) => setSort(sort)}
+                >
+                  Name
+                </SortableComponent>
+                <SortableComponent
+                  prop="title"
+                  sort={sort}
+                  onClick={(sort) => setSort(sort)}
+                >
+                  Title
+                </SortableComponent>
+                <SortableComponent
+                  prop="email"
+                  sort={sort}
+                  onClick={(sort) => setSort(sort)}
+                >
+                  Email
+                </SortableComponent>
+                <SortableComponent
+                  prop="role"
+                  sort={sort}
+                  onClick={(sort) => setSort(sort)}
+                >
+                  Role
+                </SortableComponent>
+              </tr>
             </thead>
             <tbody className=" bg-white-- divide-y divide-gray-300">
-                {sortedPeople.map(person=>{
-                    return (
-                        <tr key={person.email} className="bg-white">
-                            <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person.name}</td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{person.title}</td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{person.email}</td>
-                            <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
-                        </tr>
-                    )
-                })}
-
+              {sortedPeople.map((person) => {
+                return (
+                  <tr key={person.email} className="bg-white">
+                    <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {person.name}
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.title}
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.email}
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {person.role}
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
       </div>
-
     </div>
   );
 };
@@ -96,10 +101,10 @@ const SortableComponent: React.FC<SortableComponentProps> = ({
   onClick,
   children,
 }) => {
-  const [acceding, descending] = sort.split(":") ?? [];
+  const [acceding, descending] = sort?.split(":") ?? [];
 
   let sortElement: string | null;
-  if (!sort) {
+  if (acceding !== prop) {
     sortElement = prop;
   } else if (sort === prop) {
     sortElement = `${prop}:desc`;
@@ -108,7 +113,7 @@ const SortableComponent: React.FC<SortableComponentProps> = ({
   }
   return (
     <th
-      className="py-3.5 pl-4 pr-3 text-left text-sm text-gray-900 sm:pl-5"
+      className="py-3.5 px-3 first:pl-4  text-left text-sm text-gray-900 first:sm:pl-5"
       scope="col"
     >
       <button
@@ -118,12 +123,12 @@ const SortableComponent: React.FC<SortableComponentProps> = ({
         {children}
         <span
           className={`${
-            acceding === "name"
-              ? "text-gray-900 bg-gray-200 group-hover:bg-gray-400"
+            acceding === prop
+              ? "text-gray-900 group-hover:bg-gray-400"
               : "invisible text-gray-400 group-hover:visible "
           } flex-none ml-2 rounded`}
         >
-          <ArrowDown className={`${descending ? "-rotate-180" : ""}`} />
+          <ArrowDown className={`${descending ? "-rotate-180" : ""}`} size={16} />
         </span>{" "}
       </button>
     </th>
